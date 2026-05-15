eb3882125d99defa746a3387ebaa2669f0d14e95
Ralph' Potato <FalconPilot@users.noreply.github.com>
2026-05-15 18:40:07 +0200

    Simplified maybe API

--- src/utils/maybe.ts
+++ src/utils/maybe.ts
@@ -1,22 +1,25 @@
-type ApplyFunction<T> = <RT>(f: (value: T) => RT) => RT | null
-
 type Maybe<T> = {
-  apply: ApplyFunction<T>
-  pipe: <RT>(f: (value: T) => RT, fallback?: T) => Maybe<RT>
-  verify: (f: (value: T) => boolean, fallback?: T) => T | null
+  apply: <RT>(f: (value: T) => RT, fallback?: RT) => Maybe<RT>
+  verify: (f: (value: T) => boolean, fallback?: T) => Maybe<T>
+  unless: (f: (value: T) => boolean, fallback?: T) => Maybe<T>
+  value: T | null
 }
 
 const exists = <T>(x: T | null | undefined): x is T =>
   x !== null && x !== undefined
 
 const apply = <T>(x: T | null | undefined) => <RT>(f: (value: T) => RT, fallback?: RT) =>
-  exists(x) ? f(x) : fallback ?? null
+  maybe(exists(x) ? f(x) : fallback ?? null)
 
 const verify = <T>(x: T | null | undefined) => (f: (value: T) => boolean, fallback?: T) =>
-  exists(x) && f(x) ? x : fallback ?? null
+  maybe(exists(x) && f(x) ? x : fallback ?? null)
+
+const unless = <T>(x: T | null | undefined) => (f: (value: T) => boolean, fallback?: T) =>
+  maybe(exists(x) && !f(x) ? x : fallback ?? null)
 
 export const maybe = <T>(x: T | null | undefined): Maybe<T> => ({
   apply: apply(x),
-  pipe: <RT>(f: (value: T) => RT) => maybe<RT>(apply(x)(f)),
   verify: verify(x),
+  unless: unless(x),
+  value: x ?? null,
 })
